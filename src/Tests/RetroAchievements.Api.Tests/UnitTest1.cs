7a54bfef66eb070c5235c726bacc9a801cda60ee
krystian.lesniak <kontakt@krystianlesniak.pl>
2023-07-10 10:52:25 +0200

    Created initial Unit Test project

---
+++ src/Tests/RetroAchievements.Api.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RetroAchievements.Api.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
