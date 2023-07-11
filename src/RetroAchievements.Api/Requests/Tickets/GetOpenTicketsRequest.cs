5275f8b6108f121012999c7660e322b54ecaba04
krystian.lesniak <kontakt@krystianlesniak.pl>
2023-07-11 18:21:39 +0200

    Created GetOpenTickets

---
+++ src/RetroAchievements.Api/Requests/Tickets/GetOpenTicketsRequest.cs
@@ -0,0 +1,26 @@
+﻿
+using RetroAchievements.Api.Exceptions;
+using RetroAchievements.Api.Request;
+using RetroAchievements.Api.Response.Tickets;
+
+namespace RetroAchievements.Api.Requests.Tickets
+{
+    public record GetOpenTicketsRequest : IRetroAchievementsRequest<GetOpenTicketsResponse>
+    {
+        public string RequestEndpoint => "API_GetTicketData";
+
+        public GetOpenTicketsRequest(int offset = 0, int count = 10)
+        {
+            ArgumentExceptionGuard.ThrowIfGreaterThan(count, nameof(count), 100);
+
+            Offset = offset;
+            Count = count;
+        }
+
+        [ApiInputKey("o")]
+        public int Offset { get; init; }
+
+        [ApiInputKey("c")]
+        public int Count { get; init; }
+    }
+}
---
+++ src/RetroAchievements.Api/Response/Tickets/GetOpenTicketsResponse.cs
@@ -0,0 +1,19 @@
+﻿using RetroAchievements.Api.Response.Records;
+using System.Text.Json.Serialization;
+
+namespace RetroAchievements.Api.Response.Tickets
+{
+    public record GetOpenTicketsResponse : RetroAchievementsResponse
+    {
+
+        [JsonInclude]
+        public IEnumerable<Ticket> RecentTickets { get; private set; } = new List<Ticket>();
+
+        [JsonInclude]
+        public int OpenTickets { get; private set; }
+
+        [JsonInclude]
+        [JsonPropertyName("URL")]
+        public string Url { get; private set; } = string.Empty;
+    }
+}
