b2ec16f1f0d147623aa5759628d910ac8cb2b5a6
krystian.lesniak <kontakt@krystianlesniak.pl>
2023-07-11 17:15:44 +0200

    Created Console API

---
+++ src/RetroAchievements.Api/ConsolesApi.cs
@@ -0,0 +1,31 @@
+﻿
+using RetroAchievements.Api.Requests.Consoles;
+using RetroAchievements.Api.Response.Consoles;
+
+namespace RetroAchievements.Api
+{
+    public static class ConsolesApi
+    {
+        #region GetConsoleIds
+        public static async Task<GetConsoleIdsResponse> GetConsoleIdsAsync(this RetroAchievementsHttpClient client, RetroAchievementsAuthenticationData? authenticationData = null)
+        {
+            return await client.SendAsync(new GetConsoleIdsRequest(), authenticationData);
+        }
+        public static GetConsoleIdsResponse GetConsoleIds(this RetroAchievementsHttpClient client, RetroAchievementsAuthenticationData? authenticationData = null)
+        {
+            return client.Send(new GetConsoleIdsRequest(), authenticationData);
+        }
+        #endregion GetConsoleIds
+
+        #region GetGameList
+        public static async Task<GetGameListResponse> GetGameListAsync(this RetroAchievementsHttpClient client, int consoleId, bool onlyWithAchievements = false, bool getHashes = false, RetroAchievementsAuthenticationData? authenticationData = null)
+        {
+            return await client.SendAsync(new GetGameListRequest(consoleId, onlyWithAchievements, getHashes), authenticationData);
+        }
+        public static GetGameListResponse GetGameList(this RetroAchievementsHttpClient client, int consoleId, bool onlyWithAchievements = false, bool getHashes = false, RetroAchievementsAuthenticationData? authenticationData = null)
+        {
+            return client.Send(new GetGameListRequest(consoleId, onlyWithAchievements, getHashes), authenticationData);
+        }
+        #endregion GetGameList
+    }
+}
---
+++ src/RetroAchievements.Api/Response/Consoles/Records/Console.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RetroAchievements.Api.Response.Consoles.Records
+{
+    public record Console
+    {
+        [JsonInclude]
+        [JsonPropertyName("ID")]
+        public int Id { get; private set; }
+
+        [JsonInclude]
+        public string Name { get; private set; } = string.Empty;
+    }
+}
