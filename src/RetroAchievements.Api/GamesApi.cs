44a04fa228a0d41c0852f2515dbdaea2bc99da4c
krystian.lesniak <kontakt@krystianlesniak.pl>
2023-07-11 10:22:54 +0200

    Prepared GamesApi.cs

---
+++ src/RetroAchievements.Api/GamesApi.cs
@@ -0,0 +1,74 @@
+﻿using RetroAchievements.Api.Requests.Enums;
+
+
+namespace RetroAchievements.Api
+{
+    public static class GamesApi
+    {
+        //#region GetAchievementCount
+        //public static async Task<GetAchievementCountResponse> GetAchievementCountAsync(this RetroAchievementsHttpClient client, RetroAchievementsAuthenticationData? authenticationData = null)
+        //{
+        //    return await client.SendAsync(new GetAchievementCountRequest(), authenticationData);
+        //}
+        //public static GetAchievementCountResponse GetAchievementCount(this RetroAchievementsHttpClient client, RetroAchievementsAuthenticationData? authenticationData = null)
+        //{
+        //    return client.Send(new GetAchievementCountRequest(), authenticationData);
+        //}
+        //#endregion GetAchievementCount
+
+        //#region GetAchievementDistribution
+        //public static async Task<GetAchievementDistributionResponse> GetAchievementDistributionAsync(this RetroAchievementsHttpClient client, RetroAchievementsAuthenticationData? authenticationData = null)
+        //{
+        //    return await client.SendAsync(new GetAchievementDistributionRequest(), authenticationData);
+        //}
+        //public static GetAchievementDistributionResponse GetAchievementDistribution(this RetroAchievementsHttpClient client, RetroAchievementsAuthenticationData? authenticationData = null)
+        //{
+        //    return client.Send(new GetAchievementDistributionRequest(), authenticationData);
+        //}
+        //#endregion GetAchievementDistribution
+
+        //#region GetGame
+        //public static async Task<GetGameResponse> GetGameAsync(this RetroAchievementsHttpClient client, RetroAchievementsAuthenticationData? authenticationData = null)
+        //{
+        //    return await client.SendAsync(new GetGameRequest(), authenticationData);
+        //}
+        //public static GetGameResponse GetGame(this RetroAchievementsHttpClient client, RetroAchievementsAuthenticationData? authenticationData = null)
+        //{
+        //    return client.Send(new GetGameRequest(), authenticationData);
+        //}
+        //#endregion GetGame
+
+        //#region GetGameExtended
+        //public static async Task<GetGameExtendedResponse> GetGameExtendedAsync(this RetroAchievementsHttpClient client, RetroAchievementsAuthenticationData? authenticationData = null)
+        //{
+        //    return await client.SendAsync(new GetGameExtendedRequest(), authenticationData);
+        //}
+        //public static GetGameExtendedResponse GetGameExtended(this RetroAchievementsHttpClient client, RetroAchievementsAuthenticationData? authenticationData = null)
+        //{
+        //    return client.Send(new GetGameExtendedRequest(), authenticationData);
+        //}
+        //#endregion GetGameExtended
+
+        //#region GetGameRankAndScore
+        //public static async Task<GetGameRankAndScoreResponse> GetGameRankAndScoreAsync(this RetroAchievementsHttpClient client, RetroAchievementsAuthenticationData? authenticationData = null)
+        //{
+        //    return await client.SendAsync(new GetGameRankAndScoreRequest(), authenticationData);
+        //}
+        //public static GetGameRankAndScoreResponse GetGameRankAndScore(this RetroAchievementsHttpClient client, RetroAchievementsAuthenticationData? authenticationData = null)
+        //{
+        //    return client.Send(new GetGameRankAndScoreRequest(), authenticationData);
+        //}
+        //#endregion GetGameRankAndScore
+
+        //#region GetGameRating
+        //public static async Task<GetGameRatingResponse> GetGameRatingAsync(this RetroAchievementsHttpClient client, RetroAchievementsAuthenticationData? authenticationData = null)
+        //{
+        //    return await client.SendAsync(new GetGameRatingRequest(), authenticationData);
+        //}
+        //public static GetGameRatingResponse GetGameRating(this RetroAchievementsHttpClient client, RetroAchievementsAuthenticationData? authenticationData = null)
+        //{
+        //    return client.Send(new GetGameRatingRequest(), authenticationData);
+        //}
+        //#endregion GetGameRating
+    }
+}
