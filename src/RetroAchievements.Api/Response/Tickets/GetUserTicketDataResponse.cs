9109960cc980ef6ace616fa6687452386c3fddc8
krystian.lesniak <kontakt@krystianlesniak.pl>
2023-07-12 09:13:18 +0200

    Created "GetUserTicketData"

---
+++ src/RetroAchievements.Api/Response/Tickets/GetUserTicketDataResponse.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RetroAchievements.Api.Response.Tickets
+{
+    public record GetUserTicketDataResponse : RetroAchievementsResponse
+    {
+        [JsonInclude]
+        public string User { get; private set; } = string.Empty;
+
+        [JsonInclude]
+        public int Open { get; private set; }
+
+        [JsonInclude]
+        public int Closed { get; private set; }
+
+        [JsonInclude]
+        public int Resolved { get; private set; }
+
+        [JsonInclude]
+        public int Total { get; private set; }
+
+        [JsonInclude]
+        [JsonPropertyName("URL")]
+        public string Url { get; private set; } = string.Empty;
+    }
+}
