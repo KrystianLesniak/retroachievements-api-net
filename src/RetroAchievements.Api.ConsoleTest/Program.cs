c3eef8e650af04efeb1c94dc19206661bc04a40a
krystian.lesniak <kontakt@krystianlesniak.pl>
2023-07-12 14:46:35 +0200

    Replaced IEnumerable with IReadOnlyCollection in CollectionRetroAchievementsResponse

--- src/RetroAchievements.Api.ConsoleTest/Program.cs
+++ src/RetroAchievements.Api.ConsoleTest/Program.cs
@@ -44,5 +44,5 @@ using (var client = new RetroAchievementsHttpClient(authData))
     //    await Task.Delay(500);
 
     //}
-    var response = await client.GetAchievementOfTheWeekAsync();
+    var response = await client.GetConsoleIdsAsync();
 };
