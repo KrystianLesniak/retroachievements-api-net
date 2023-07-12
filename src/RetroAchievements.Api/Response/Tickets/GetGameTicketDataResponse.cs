8ec71d9c6f8f269c663ac985d13b6616b1eb405d
krystian.lesniak <kontakt@krystianlesniak.pl>
2023-07-12 09:29:20 +0200

    Created "GetGameTicketData"

---
+++ src/RetroAchievements.Api/Response/Tickets/GetGameTicketDataResponse.cs
@@ -0,0 +1,29 @@
+﻿using RetroAchievements.Api.Response.Records;
+using System.Text.Json.Serialization;
+
+namespace RetroAchievements.Api.Response.Tickets
+{
+    public record GetGameTicketDataResponse : RetroAchievementsResponse
+    {
+        [JsonInclude]
+        [JsonPropertyName("GameID")]
+        public int GameId { get; private set; }
+
+        [JsonInclude]
+        public string GameTitle { get; private set; } = string.Empty;
+
+        [JsonInclude]
+        public string ConsoleName { get; private set; } = string.Empty;
+
+        [JsonInclude]
+        public int OpenTickets { get; private set; }
+
+        [JsonInclude]
+        [JsonPropertyName("URL")]
+        public string Url { get; private set; } = string.Empty;
+
+        [JsonInclude]
+        public IEnumerable<Ticket> Tickets { get; private set; } = new List<Ticket>();
+
+    }
+}
