4e000cc7c2f9a0348b69c00bfb91144690202533
krystian.lesniak <kontakt@krystianlesniak.pl>
2023-07-12 12:30:48 +0200

    Added API_GetAchievementsEarnedOnDay XML comments

--- src/RetroAchievements.Api/Requests/Users/GetAchievementsEarnedOnDayRequest.cs
+++ src/RetroAchievements.Api/Requests/Users/GetAchievementsEarnedOnDayRequest.cs
@@ -3,10 +3,17 @@ using RetroAchievements.Api.Response.Users;
 
 namespace RetroAchievements.Api.Requests.Users
 {
+    /// <summary>
+    /// Get achievements earned by a user on specified date.
+    /// </summary>
     public record GetAchievementsEarnedOnDayRequest : IRetroAchievementsRequest<GetAchievementsEarnedOnDayResponse>
     {
+        /// <inheritdoc />
         public string RequestEndpoint => "API_GetAchievementsEarnedOnDay";
 
+        ///<inheritdoc cref="GetAchievementsEarnedOnDayRequest" />
+        ///<param name="user"><inheritdoc cref="User" path="/summary/node()"/></param>
+        ///<param name="date"><inheritdoc cref="Date" path="/summary/node()"/></param>
         public GetAchievementsEarnedOnDayRequest(string user, DateTime date)
         {
             ArgumentNullException.ThrowIfNull(user);
@@ -15,9 +22,15 @@ namespace RetroAchievements.Api.Requests.Users
             Date = date;
         }
 
+        /// <summary>
+        /// Username of a user.
+        /// </summary>
         [ApiInputKey("u")]
         public string User { get; init; }
 
+        /// <summary>
+        /// Date when achievements was earned.
+        /// </summary>
         [ApiInputKey("d", CustomOption.DateTimeToShortDate)]
         public DateTime Date { get; init; }
     }
