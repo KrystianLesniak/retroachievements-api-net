93521dc16ae8c64ba6c37a96212245e5e75c8b45
krystian.lesniak <kontakt@krystianlesniak.pl>
2023-07-30 20:47:05 +0200

    Implemented DelayHttpClient handler for Unit Tests

--- src/Tests/RetroAchievements.Api.Tests.Common/TestHttpClient.cs
+++ src/Tests/RetroAchievements.Api.Tests.Common/TestHttpClient.cs
@@ -8,11 +8,7 @@
         {
             if(HttpClient == null)
             {
-                var netHttpClient = HttpClientFactory.Create(new HttpClientHandler()
-                {
-                    MaxConnectionsPerServer = 1
-                }, 
-                new DelayHandler());
+                var netHttpClient = HttpClientFactory.Create(new DelayHandler());
 
                 if (authData == null)
                     HttpClient = new RetroAchievementsHttpClient(netHttpClient, TestAuthenticationData.CreateFromSecrets());
@@ -26,12 +22,49 @@
 
     }
 
+    //RetroAchievements upon many requests send back HTTP Error 429 = Too many requests
+    //This exists to prevent it so all API Tests may be run in parallel but request are not send simultaneously
+    //It's not really betufil solution, might be written better but it's only related to Tests performance ;p
     public class DelayHandler : DelegatingHandler
     {
+        private bool IsRequestOngoing = false;
+
+        private const int RequestDelayMiliseconds = 75;
+        private readonly int RequestAwaitMilisecond = RequestDelayMiliseconds / Environment.ProcessorCount;
+
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            await Task.Delay(100, cancellationToken);
-            return await base.SendAsync(request, cancellationToken);
+            while(IsRequestOngoing)
+            {
+                await Task.Delay(RequestAwaitMilisecond, cancellationToken);
+            }
+
+            IsRequestOngoing = true;
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            await Task.Delay(RequestDelayMiliseconds, cancellationToken);
+
+            IsRequestOngoing = false;
+            return response;
+        }
+
+        protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            while (IsRequestOngoing)
+            {
+                Thread.Sleep(RequestAwaitMilisecond);
+            }
+
+            IsRequestOngoing = true;
+
+            var response = base.Send(request, cancellationToken);
+
+            Thread.Sleep(RequestDelayMiliseconds);
+
+            IsRequestOngoing = false;
+
+            return response;
         }
     }
 }
