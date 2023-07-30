8bc29b382143ac66be4dc4d28875c8c2bbeea3dd
krystian.lesniak <kontakt@krystianlesniak.pl>
2023-07-30 15:14:26 +0200

    Created Unit Tests for Consoles

---
+++ src/Tests/RetroAchievements.Api.Consoles.Tests/GetConsoleIdsTests.cs
@@ -0,0 +1,27 @@
+using RetroAchievements.Api.Requests.Consoles;
+
+namespace RetroAchievements.Api.Consoles.Tests
+{
+    public class GetConsoleIdsTests
+    {
+        public RetroAchievementsHttpClient HttpClient = TestHttpClient.GetRetroAchievementsApiClient();
+
+        [Test]
+        public async Task GetConsoleIds_ReturnsProperResponse()
+        {
+            var responseMethodAsync = await HttpClient.GetConsoleIdsAsync();
+            var responseMethodSync = HttpClient.GetConsoleIds();
+            var responseAsync = await HttpClient.SendAsync(new GetConsoleIdsRequest());
+            var responseSync = HttpClient.Send(new GetConsoleIdsRequest());
+
+            AssertResponses.AreEqual(responseMethodAsync, responseMethodSync, responseAsync, responseSync);
+            Assert.Multiple(() =>
+            {
+                Assert.That(responseMethodAsync.HttpStatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(responseMethodAsync.FailedResponseString, Is.EqualTo(null));
+                Assert.That(responseSync.Items.Any());
+                Assert.That(responseSync.Items.Any(x=> x.Id == 1));
+            });
+        }
+    }
+}
---
+++ src/Tests/RetroAchievements.Api.Consoles.Tests/GetGamesListTests.cs
@@ -0,0 +1,94 @@
+using RetroAchievements.Api.Requests.Consoles;
+
+namespace RetroAchievements.Api.Consoles.Tests
+{
+    public class GetGamesListTests
+    {
+        public RetroAchievementsHttpClient HttpClient = TestHttpClient.GetRetroAchievementsApiClient();
+
+        [Test]
+        public async Task GetGamesList_ReturnsProperResponse()
+        {
+            int consoleId = 1;
+
+            var responseMethodAsync = await HttpClient.GetGamesListAsync(consoleId);
+            var responseMethodSync = HttpClient.GetGamesList(consoleId);
+            var responseAsync = await HttpClient.SendAsync(new GetGamesListRequest(consoleId));
+            var responseSync = HttpClient.Send(new GetGamesListRequest(consoleId));
+
+            AssertResponses.AreEqual(responseMethodAsync, responseMethodSync, responseAsync, responseSync);
+            Assert.Multiple(() =>
+            {
+                Assert.That(responseMethodAsync.HttpStatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(responseMethodAsync.FailedResponseString, Is.EqualTo(null));
+                Assert.That(responseSync.Items.Any());
+            });
+        }
+
+        [Test]
+        public async Task GetGamesList_WithoutHashes_ReturnResponseWithoutHashes()
+        {
+            int consoleId = 1;
+
+            var response = await HttpClient.GetGamesListAsync(consoleId);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(response.HttpStatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(response.FailedResponseString, Is.EqualTo(null));
+                Assert.That(response.Items.Any());
+                Assert.That(response.Items.All(x => !x.Hashes.Any()));
+            });
+        }
+
+        [Test]
+        public async Task GetGamesList_WithHashes_ReturnResponseWithHashes()
+        {
+            int consoleId = 1;
+
+            var response = await HttpClient.GetGamesListAsync(consoleId, getHashes: true);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(response.HttpStatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(response.FailedResponseString, Is.EqualTo(null));
+                Assert.That(response.Items.Any());
+                Assert.That(response.Items.All(x => x.Hashes.All(x => x != string.Empty)));
+            });
+        }
+
+        [Test]
+        public async Task GetGamesList_OnlyWithAchievements_ReturnResponseWithAchievementsOnly()
+        {
+            int consoleId = 1;
+
+            var response = await HttpClient.GetGamesListAsync(consoleId, onlyWithAchievements: true);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(response.HttpStatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(response.FailedResponseString, Is.EqualTo(null));
+                Assert.That(response.Items.All(x=> x.AchievementsCount > 0));
+            });
+        }
+
+        [Test]
+        public async Task GetGamesList_ReturnsEmptyResponse()
+        {
+            int consoleId = 100000000;
+
+            var responseMethodAsync = await HttpClient.GetGamesListAsync(consoleId);
+            var responseMethodSync = HttpClient.GetGamesList(consoleId);
+            var responseAsync = await HttpClient.SendAsync(new GetGamesListRequest(consoleId));
+            var responseSync = HttpClient.Send(new GetGamesListRequest(consoleId));
+
+            AssertResponses.AreEqual(responseMethodAsync, responseMethodSync, responseAsync, responseSync);
+            Assert.Multiple(() =>
+            {
+                Assert.That(responseMethodAsync.HttpStatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(responseMethodAsync.FailedResponseString, Is.EqualTo(null));
+                Assert.That(!responseMethodSync.Items.Any());
+            });
+        }
+    }
+}
